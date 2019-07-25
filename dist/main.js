29ae53fba5c6062873ee522b94b97d8cde22e928
nilarnab mookherjee <niluriju@gmail.com>
2019-07-25 12:11:14 +0530

    refactor

--- dist/main.js
+++ dist/main.js
@@ -112,7 +112,96 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-var computeGridLayout = function computeGridLayout(domTree) {
+var parseTemplete = function parseTemplete(template) {
+  return template.map(function (size, index) {
+    return {
+      size: size,
+      start: index + 1,
+      end: index + 2
+    };
+  });
+},
+    withinBounds = function withinBounds(rowStart, rowEnd, colStart, colEnd, templateRows, templateColumns) {
+  return rowStart - 1 >= 0 && rowStart - 1 < templateRows.length && rowEnd - 2 >= 0 && rowEnd - 2 < templateRows.length && colStart - 1 >= 0 && colStart - 1 < templateColumns.length && colEnd - 2 >= 0 && colEnd - 2 < templateColumns.length;
+},
+    addCoordinatesToCells = function addCoordinatesToCells(gridMatrix, children) {
+  var i,
+      j,
+      item,
+      usedX = 0,
+      usedY = 0,
+      cell = {};
+
+  for (i = 0; i < gridMatrix.length; i++) {
+    usedX = 0; // usedY = null;
+
+    for (j = 0; j < gridMatrix[i].length; j++) {
+      item = gridMatrix[i][j];
+      item.startX = usedX;
+      item.endX = usedX + item.columnSize;
+      usedX = item.endX;
+      item.startY = usedY;
+      item.endY = usedY + item.rowSize;
+
+      if (j == gridMatrix[i].length - 1) {
+        usedY = usedY + item.rowSize;
+      }
+    }
+  }
+
+  children.forEach(function (child) {
+    cell = gridMatrix[child.matrixPosition.row][child.matrixPosition.column];
+    child.layout = {};
+    child.layout.width = child.style.width;
+    child.layout.height = child.style.height;
+    child.layout.startX = cell.startX;
+    child.layout.startY = cell.startY;
+    child.layout.endX = cell.endX;
+    child.layout.endY = cell.endY;
+  });
+},
+    placeChildrenInGrid = function placeChildrenInGrid(children, gridMatrix, templateRows, templateColumns) {
+  children.forEach(function (child) {
+    var _child$style = child.style,
+        gridRowStart = _child$style.gridRowStart,
+        gridRowEnd = _child$style.gridRowEnd,
+        gridColumnStart = _child$style.gridColumnStart,
+        gridColumnEnd = _child$style.gridColumnEnd;
+
+    if (withinBounds(gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd, templateRows, templateColumns)) {
+      gridMatrix[gridRowStart - 1][gridColumnStart - 1].item = child;
+      child.matrixPosition = {
+        row: gridRowStart - 1,
+        column: gridColumnStart - 1
+      }; // TODO:  consider spanning items
+    }
+  });
+},
+    inflateGridCells = function inflateGridCells(tsa, children, columns, rows, containerHeight, containerWidth, gridMatrix) {
+  var i, j;
+  columns = tsa.set('tracks', columns).set('items', children.map(function (c) {
+    return {
+      start: c.style.gridColumnStart,
+      end: c.style.gridColumnEnd,
+      size: c.style.width
+    };
+  })).set('containerSize', containerHeight).resolveTracks();
+  rows = tsa.set('tracks', rows).set('items', children.map(function (r) {
+    return {
+      start: r.style.gridRowStart,
+      end: r.style.gridRowEnd,
+      size: r.style.height
+    };
+  })).set('containerSize', containerWidth).resolveTracks();
+
+  for (i = 0; i < rows.length; i++) {
+    for (j = 0; j < rows.length; j++) {
+      gridMatrix[i][j].rowSize = rows[i].baseSize;
+      gridMatrix[i][j].columnSize = columns[j].baseSize;
+    }
+  }
+},
+    computeGridLayout = function computeGridLayout(domTree) {
   var gridMatrix = [],
       styles = domTree.style || {},
       children = domTree.children || [],
@@ -120,52 +209,7 @@ var computeGridLayout = function computeGridLayout(domTree) {
       templateColumns = styles.templateColumns,
       width = styles.width,
       height = styles.height,
-      parseTemplete = function parseTemplete(template) {
-    return template.map(function (size, index) {
-      return {
-        size: size,
-        start: index + 1,
-        end: index + 2
-      };
-    });
-  },
       tsa = new _track_sizing__WEBPACK_IMPORTED_MODULE_2__["default"](),
-      withinBounds = function withinBounds(rowStart, rowEnd, colStart, colEnd) {
-    return rowStart - 1 >= 0 && rowStart - 1 < templateRows.length && rowEnd - 2 >= 0 && rowEnd - 2 < templateRows.length && colStart - 1 >= 0 && colStart - 1 < templateColumns.length && colEnd - 2 >= 0 && colEnd - 2 < templateColumns.length;
-  },
-      addCoordinatesToCells = function addCoordinatesToCells() {
-    var i,
-        j,
-        item,
-        usedX = 0,
-        usedY = 0,
-        cell = {};
-
-    for (i = 0; i < gridMatrix.length; i++) {
-      usedX = 0; // usedY = null;
-
-      for (j = 0; j < gridMatrix[i].length; j++) {
-        item = gridMatrix[i][j];
-        item.startX = usedX;
-        item.endX = usedX + item.columnSize;
-        usedX = item.endX;
-        item.startY = usedY;
-        item.endY = usedY + item.rowSize;
-
-        if (j == gridMatrix[i].length - 1) {
-          usedY = usedY + item.rowSize;
-        }
-      }
-    }
-
-    children.forEach(function (child) {
-      cell = gridMatrix[child.matrixPosition.row][child.matrixPosition.column];
-      child.startX = cell.startX;
-      child.startY = cell.startY;
-      child.endX = cell.endX;
-      child.endY = cell.endY;
-    });
-  },
       createGridMatrix = function createGridMatrix() {
     var i;
 
@@ -176,49 +220,6 @@ var computeGridLayout = function computeGridLayout(domTree) {
           rowSize: +formattedRows[i].size
         };
       }));
-    }
-  },
-      placeChildrenInGrid = function placeChildrenInGrid() {
-    children.forEach(function (child) {
-      var _child$style = child.style,
-          gridRowStart = _child$style.gridRowStart,
-          gridRowEnd = _child$style.gridRowEnd,
-          gridColumnStart = _child$style.gridColumnStart,
-          gridColumnEnd = _child$style.gridColumnEnd;
-
-      if (withinBounds(gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd)) {
-        gridMatrix[gridRowStart - 1][gridColumnStart - 1].item = child;
-        child.matrixPosition = {
-          row: gridRowStart - 1,
-          column: gridColumnStart - 1
-        }; // TODO:  consider spanning items
-      }
-    });
-  },
-      inflateGridCells = function inflateGridCells() {
-    var rowsWithSize, columnsWithSize, i, j;
-    columnsWithSize = tsa.set('tracks', formattedColumns).set('items', children.map(function (c) {
-      return {
-        start: c.style.gridColumnStart,
-        end: c.style.gridColumnEnd,
-        size: c.style.width
-      };
-    })).set('containerSize', height).resolveTracks();
-    formattedColumns = columnsWithSize;
-    rowsWithSize = tsa.set('tracks', formattedRows).set('items', children.map(function (r) {
-      return {
-        start: r.style.gridRowStart,
-        end: r.style.gridRowEnd,
-        size: r.style.height
-      };
-    })).set('containerSize', width).resolveTracks();
-    formattedRows = rowsWithSize;
-
-    for (i = 0; i < rowsWithSize.length; i++) {
-      for (j = 0; j < rowsWithSize.length; j++) {
-        gridMatrix[i][j].rowSize = rowsWithSize[i].baseSize;
-        gridMatrix[i][j].columnSize = columnsWithSize[j].baseSize;
-      }
     }
   };
 
@@ -235,13 +236,13 @@ var computeGridLayout = function computeGridLayout(domTree) {
 
   createGridMatrix(); // Allocate children to grid matrix
 
-  placeChildrenInGrid(); // Size the rows and tracks
+  placeChildrenInGrid(children, gridMatrix, templateRows, templateColumns); // Size the rows and tracks
 
-  inflateGridCells(); // Calling this second time to ensure that if some item's min-content-contribution has changed
+  inflateGridCells(tsa, children, formattedColumns, formattedRows, height, width, gridMatrix); // Calling this second time to ensure that if some item's min-content-contribution has changed
 
-  inflateGridCells(); // Adds x,y coordinates to each cell depending on position and dimensions
+  inflateGridCells(tsa, children, formattedColumns, formattedRows, height, width, gridMatrix); // Adds x,y coordinates to each cell depending on position and dimensions
 
-  addCoordinatesToCells();
+  addCoordinatesToCells(gridMatrix, children);
   return domTree;
 };
 
--- src/grid/index.js
+++ src/grid/index.js
@@ -2,122 +2,122 @@ import { getDisplayProperty } from "../utils";
 import { computeLayout } from "../mason";
 import TrackResolver from "./track-sizing";
 
-const computeGridLayout = (domTree) => {
-  const gridMatrix = [],
-    styles = domTree.style || {},
-    children = domTree.children || [],
-    { templateRows, templateColumns, width, height } = styles,
-    parseTemplete = (template) => template.map((size, index) => ({ size, start: index + 1, end: index + 2 })),
-    tsa = new TrackResolver(),
-    withinBounds = (rowStart, rowEnd, colStart, colEnd) => {
-      return rowStart - 1 >= 0
-        && rowStart - 1 < templateRows.length
-        && rowEnd - 2 >= 0
-        && rowEnd - 2 < templateRows.length
-        && colStart - 1 >= 0
-        && colStart - 1 < templateColumns.length
-        && colEnd - 2 >= 0
-        && colEnd - 2 < templateColumns.length;
-    },
-    addCoordinatesToCells = () => {
-      let i, j, item, usedX = 0, usedY = 0, cell = {};
-      for (i = 0; i < gridMatrix.length; i++) {
-        usedX = 0;
-        // usedY = null;
-        for (j = 0; j < gridMatrix[i].length; j++) {
-          item = gridMatrix[i][j];
 
-          item.startX = usedX;
-          item.endX = usedX + item.columnSize;
-          usedX = item.endX;
+const parseTemplete = (template) => template.map((size, index) => ({ size, start: index + 1, end: index + 2 })),
+  withinBounds = (rowStart, rowEnd, colStart, colEnd, templateRows, templateColumns) => {
+    return rowStart - 1 >= 0
+      && rowStart - 1 < templateRows.length
+      && rowEnd - 2 >= 0
+      && rowEnd - 2 < templateRows.length
+      && colStart - 1 >= 0
+      && colStart - 1 < templateColumns.length
+      && colEnd - 2 >= 0
+      && colEnd - 2 < templateColumns.length;
+  },
+  addCoordinatesToCells = (gridMatrix, children) => {
+    let i, j, item, usedX = 0, usedY = 0, cell = {};
+    for (i = 0; i < gridMatrix.length; i++) {
+      usedX = 0;
+      // usedY = null;
+      for (j = 0; j < gridMatrix[i].length; j++) {
+        item = gridMatrix[i][j];
 
-          item.startY = usedY;
-          item.endY = usedY + item.rowSize;
+        item.startX = usedX;
+        item.endX = usedX + item.columnSize;
+        usedX = item.endX;
 
-          if (j == gridMatrix[i].length - 1) {
-            usedY = usedY + item.rowSize;
-          }
+        item.startY = usedY;
+        item.endY = usedY + item.rowSize;
+
+        if (j == gridMatrix[i].length - 1) {
+          usedY = usedY + item.rowSize;
         }
       }
-
-      children.forEach(child => {
-        cell = gridMatrix[child.matrixPosition.row][child.matrixPosition.column];
-        child.startX = cell.startX;
-        child.startY = cell.startY;
-        child.endX = cell.endX;
-        child.endY = cell.endY;
-      });
-    },
-    createGridMatrix = () => {
-      let i;
-      for (i = 0; i < formattedRows.length; i++) {
-        gridMatrix.push(formattedColumns.map(c => ({
-          columnSize: +c.size,
-          rowSize: +formattedRows[i].size
-        })));
-      }
-    },
-    placeChildrenInGrid = () => {
-      children.forEach(child => {
-        const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = child.style;
-        if (withinBounds(gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd)) {
-          gridMatrix[gridRowStart - 1][gridColumnStart - 1].item = child;
-          child.matrixPosition = {
-            row: gridRowStart - 1,
-            column: gridColumnStart - 1
-          };
-          // TODO:  consider spanning items
-        }
-      });
-    },
-    inflateGridCells = () => {
-      let rowsWithSize,
-        columnsWithSize,
-        i,
-        j;
-
-      columnsWithSize = tsa.set('tracks', formattedColumns)
-        .set('items', children.map(c => ({ start: c.style.gridColumnStart, end: c.style.gridColumnEnd, size: c.style.width })))
-        .set('containerSize', height)
-        .resolveTracks();
-      formattedColumns = columnsWithSize;
-
-      rowsWithSize = tsa.set('tracks', formattedRows)
-        .set('items', children.map(r => ({ start: r.style.gridRowStart, end: r.style.gridRowEnd, size: r.style.height })))
-        .set('containerSize', width)
-        .resolveTracks();
-      formattedRows = rowsWithSize;
-
-      for (i = 0; i < rowsWithSize.length; i++) {
-        for (j = 0; j < rowsWithSize.length; j++) {
-          gridMatrix[i][j].rowSize = rowsWithSize[i].baseSize;
-          gridMatrix[i][j].columnSize = columnsWithSize[j].baseSize;
-        }
-      }
-    };
-
-  let i,
-    formattedRows = parseTemplete(templateRows),
-    formattedColumns = parseTemplete(templateColumns);
-    
-  for (i = 0; i < children.length; i++) {
-    if (getDisplayProperty(children[i])) {
-      children[i] = computeLayout(children[i]);
     }
-  }
 
-  // Create the grid matrix
-  createGridMatrix();
-  // Allocate children to grid matrix
-  placeChildrenInGrid();
-  // Size the rows and tracks
-  inflateGridCells();
-  // Calling this second time to ensure that if some item's min-content-contribution has changed
-  inflateGridCells();
-  // Adds x,y coordinates to each cell depending on position and dimensions
-  addCoordinatesToCells();
+    children.forEach(child => {
+      cell = gridMatrix[child.matrixPosition.row][child.matrixPosition.column];
+      child.layout = {};
+      child.layout.width = child.style.width;
+      child.layout.height = child.style.height;
+      child.layout.startX = cell.startX;
+      child.layout.startY = cell.startY;
+      child.layout.endX = cell.endX;
+      child.layout.endY = cell.endY;
+    });
+  },
+  placeChildrenInGrid = (children, gridMatrix, templateRows, templateColumns) => {
+    children.forEach(child => {
+      const { gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd } = child.style;
+      if (withinBounds(gridRowStart, gridRowEnd, gridColumnStart, gridColumnEnd, templateRows, templateColumns)) {
+        gridMatrix[gridRowStart - 1][gridColumnStart - 1].item = child;
+        child.matrixPosition = {
+          row: gridRowStart - 1,
+          column: gridColumnStart - 1
+        };
+        // TODO:  consider spanning items
+      }
+    });
+  },
+  inflateGridCells = (tsa, children, columns, rows, containerHeight, containerWidth, gridMatrix) => {
+    let i,
+      j;
 
-  return domTree;
-};
+    columns = tsa.set('tracks', columns)
+      .set('items', children.map(c => ({ start: c.style.gridColumnStart, end: c.style.gridColumnEnd, size: c.style.width })))
+      .set('containerSize', containerHeight)
+      .resolveTracks();
+
+    rows = tsa.set('tracks', rows)
+      .set('items', children.map(r => ({ start: r.style.gridRowStart, end: r.style.gridRowEnd, size: r.style.height })))
+      .set('containerSize', containerWidth)
+      .resolveTracks();
+
+    for (i = 0; i < rows.length; i++) {
+      for (j = 0; j < rows.length; j++) {
+        gridMatrix[i][j].rowSize = rows[i].baseSize;
+        gridMatrix[i][j].columnSize = columns[j].baseSize;
+      }
+    }
+  },
+  computeGridLayout = (domTree) => {
+    const gridMatrix = [],
+      styles = domTree.style || {},
+      children = domTree.children || [],
+      { templateRows, templateColumns, width, height } = styles,
+      tsa = new TrackResolver(),
+      createGridMatrix = () => {
+        let i;
+        for (i = 0; i < formattedRows.length; i++) {
+          gridMatrix.push(formattedColumns.map(c => ({
+            columnSize: +c.size,
+            rowSize: +formattedRows[i].size
+          })));
+        }
+      };
+
+    let i,
+      formattedRows = parseTemplete(templateRows),
+      formattedColumns = parseTemplete(templateColumns);
+
+    for (i = 0; i < children.length; i++) {
+      if (getDisplayProperty(children[i])) {
+        children[i] = computeLayout(children[i]);
+      }
+    }
+
+    // Create the grid matrix
+    createGridMatrix();
+    // Allocate children to grid matrix
+    placeChildrenInGrid(children, gridMatrix, templateRows, templateColumns);
+    // Size the rows and tracks
+    inflateGridCells(tsa, children, formattedColumns, formattedRows, height, width, gridMatrix);
+    // Calling this second time to ensure that if some item's min-content-contribution has changed
+    inflateGridCells(tsa, children, formattedColumns, formattedRows, height, width, gridMatrix);
+    // Adds x,y coordinates to each cell depending on position and dimensions
+    addCoordinatesToCells(gridMatrix, children);
+
+    return domTree;
+  };
 
 export default computeGridLayout;
