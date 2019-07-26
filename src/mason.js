34062059da3cf3a58d82736b6f788b3aea4ad3ec
nilarnab mookherjee <niluriju@gmail.com>
2019-07-26 18:48:43 +0530

    Restructured Grid API

--- src/mason.js
+++ src/mason.js
@@ -1,20 +1,37 @@
-import { getDisplayProperty, cloneObject, getComputeFn, attachLayoutInformation } from "./utils";
+import { getDisplayProperty, cloneObject, attachLayoutInformation } from "./utils";
+import { DISPLAY_GRID, DISPLAY_FLEX } from "./utils/constants";
+import { computeGridLayout } from "./grid";
 
-const computeLayoutHelper = domTree => {
-    return getComputeFn(getDisplayProperty(domTree))(domTree);
-  },
-  computeLayout = (domTree = {}) => {
-    let clonedDomTree = cloneObject(domTree),
-      calculatedTree;
+class LayoutEngine {
+  constructor () {
+    this.gridLayoutEngine = computeGridLayout;
+  }
 
-    clonedDomTree.root = true;
-    calculatedTree = computeLayoutHelper(clonedDomTree);
-    attachLayoutInformation(domTree, calculatedTree);
+  compute (domTree) {
+    switch (getDisplayProperty(domTree)) {
+    
+    case DISPLAY_GRID: return this.gridLayoutEngine(domTree);
+    case DISPLAY_FLEX: return this.gridLayoutEngine(domTree);
+    default:
+      // Probably throw unsupported error?
+      return this.gridLayoutEngine(domTree);
 
-    return domTree;
-  };
+    }
+  }
+}
+
+const computeLayout = (domTree) => {
+  const mason = new LayoutEngine();
+  let clonedDomTree = cloneObject(domTree),
+    calculatedTree;
+
+  clonedDomTree.root = true;
+  calculatedTree = mason.compute(clonedDomTree);
+  attachLayoutInformation(domTree, calculatedTree);
+
+  return domTree;
+};
 
 export {
-  computeLayout,
-  computeLayoutHelper
+  computeLayout
 };
